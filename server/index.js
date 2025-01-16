44158d7d8822b9576c874f7906510a20fec7d1b4
fran <falcoba@picallex.com>
2025-01-16 10:36:30 -0300

    [React-Fullstack] - feat: Add Comment To Post

--- server/index.js
+++ server/index.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const app = express();
-const cors =require("cors")
+const cors = require("cors");
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 const db = require("./models");
 
 // Routers
 const postRouter = require("./routes/Posts");
 app.use("/posts", postRouter);
+
+// Routers
+const commentsRoute = require("./routes/Comments");
+app.use("/comments", commentsRoute);
 
 db.sequelize.sync().then(() => {
   app.listen(3001, () => {
